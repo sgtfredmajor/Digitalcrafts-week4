bd72dc860d5f07c41c4676d0f4e47f6ecadd86b0
Fred Major <59883908+sgtfredmajor@users.noreply.github.com>
2020-02-06 13:42:50 -0500

    Delete test.js

--- Madlibs/test.js
+++
@@ -1,8 +0,0 @@
-console.log(ducument.domain);
-console.log(ducument.url);
-// console.log(document.title);
-// console.log(document.doctype);
-// console.log(document.body);
-// console.log(dicument.console);
-
-// console.log(document.forms[0]);
